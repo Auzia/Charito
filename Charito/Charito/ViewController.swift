2226ab47d5de99fbcc4e5ca3c26e1760019aaf2b
Ameat77 <amit.krishnaiyer@gmail.com>
2018-07-27 21:29:57 -0400

    Initial commit
    This is the initial commit

---
+++ Charito/Charito/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Charito
+//
+//  Created by Amit on 7/27/18.
+//  Copyright © 2018 Auzia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
