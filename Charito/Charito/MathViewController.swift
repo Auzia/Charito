8c5139a188c56f9f9ecfd2d98904b14f1cde5dd8
Anika Joshi <anikaj10s@gmail.com>
2018-08-23 21:19:36 -0400

    Added iosMath

--- Charito/Charito/MathViewController.swift
+++ Charito/Charito/MathViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MathViewController.swift
 //  Charito
 //
 //  Created by Amit on 7/27/18.
@@ -7,14 +7,23 @@
 //
 
 import UIKit
+import iosMath
 
-class ViewController: UIViewController {
-
+class MathViewController: UIViewController {
+    @IBOutlet weak var labelMathTest: MTMathUILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+        // Do any additional setup after loading the view, typically from a nib.
+
+        labelMathTest.latex = "(a_1 + a_2)^2 = a_1^2 + 2a_1a_2 + a_2^2"
+        labelMathTest.sizeToFit()
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
