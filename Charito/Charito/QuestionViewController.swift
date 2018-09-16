f3cf7fe2f686489f2988c3f4e990676086e2d8e0
Kushan Weerakoon <kushanweerakoon3@gmail.com>
2018-09-16 17:59:57 -0400

    added class

--- Charito/Charito/QuestionViewController.swift
+++ Charito/Charito/QuestionViewController.swift
@@ -58,10 +58,10 @@ class QuestionViewController: UIViewController {
         }
         
         // To test a specific question, uncomment the following line
-        questionIndex = 7  // Fill in the index of the question you want to test
+        questionIndex = 0  // Fill in the index of the question you want to test
         
         let pools = QuestionPool()
-        let pool = pools.eleventhTwelfthQuestionPool
+        let pool = pools.ninthTenthQuestionPool
         
         //Make sure you track all asked questions
         answeredQs.append(questionIndex)
---
+++ Charito/Charito/TopicViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TopicViewController.swift
+//  Charito
+//
+//  Created by Kushan Weerakoon on 9/16/18.
+//  Copyright © 2018 Auzia. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TopicViewController: UIViewController{
+    
+}
