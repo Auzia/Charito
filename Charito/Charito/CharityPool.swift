3a53e472852761d63cbecb188de6e5d24f4f53d5
Anika Joshi <anikaj10s@gmail.com>
2018-10-06 15:48:51 -0400

    added sponsor class
    and other charity descriptions

--- Charito/Charito/CharityPool.swift
+++ Charito/Charito/CharityPool.swift
@@ -15,4 +15,16 @@ class CharityPool{
     public let Cap = Charity(name: "Capital Area Food Bank", description: "Capital Area Food Bank is a local food bank located in the DC Metro area that provides food to those who need it. Capital Area Food Bank is a non-profit organization and can use all the help it can get!")
     
     public let stJudes = Charity(name: "St. Jude's", description: "St.Judes is a hospital that help kids through their toughest times. St. Jude's has lots of great donors, but could use many more. Each dollar you donate goes towards saving a life!")
+    
+    public let outdoorAlliance = Charity(name: "Outdoor Alliance", description: "Outdoor Alliance protects nature and promotes conservation.")
+    
+    public let feedingAmerica = Charity(name: "Feeding America", description: "Feeding America is working to make America hunger free. It is the third largest charity in the United States.")
+    
+    public let challengerCenter = Charity(name: "Challenger Center", description: "The Challenger Center is an organization that educates students across America on space technology and real life skills such as collaboration.")
+    
+    public let endangeredSpeciesCoalition = Charity(name: "Endangered Species Coalition", description: "The Endangered Species Coalition is working to protect endangered species and prevent their extinction. They also educate people about how to protect the environment.")
+    
+     public let animalRescueCorps = Charity(name: "Animal Rescue Corps", description: "The Animal Rescue Corps are a non-profit organization that are working to end animal suffering. They rescue animals, educate pet shelters, and create public awareness about these issues.")
+    
+     public let worldWildlifeFund = Charity(name: "World Wildlife Fund", description: "The World Wildlife Fund is the world's leading conservation program. They are working to preserve the environment and deliver innovative solutions on conservancy issues.")
 }
